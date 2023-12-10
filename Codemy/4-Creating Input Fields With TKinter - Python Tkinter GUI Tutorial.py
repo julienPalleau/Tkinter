3345dfbe118cba4a51be2a318bf79cca41182629
julienPalleau <jpalleau@gmail.com>
2023-12-10 21:14:40 +0100

    4 - Creating Input Fields With TKinter - Python Tkinter GUI Tutorial

---
+++ Codemy/4-Creating Input Fields With TKinter - Python Tkinter GUI Tutorial.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+
+#############
+# input box #
+#############
+e = Entry(root, width=50, bg="blue", fg="white", borderwidth=5)
+e.pack()
+
+
+def myClick():
+    myLabel = Label(root, text=f"Hello {e.get()}")
+    myLabel.pack()
+
+
+myButton = Button(root, text="Enter Your Name", command=myClick)
+myButton.pack()
+
+root.mainloop()
