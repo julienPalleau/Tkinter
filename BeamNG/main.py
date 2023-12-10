6d421d177b1d3138b56ac49c49144782d20465a7
julienPalleau <jpalleau@gmail.com>
2023-12-10 19:55:33 +0100

    Add files via upload

---
+++ BeamNG/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+fen = Tk()
+fen.geometry("200x500")
+fen.resizable(width=False, height=False)
+# fen.attributes('-fullscreen', 1) # ouvre la fenetre en plein ecran
+fen.configure(bg="red")
+fen.title("fenetre")
+
+fen.mainloop()
